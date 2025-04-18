5c031ab54cc1b024ef6b80cbec0fe27a1c4e36e6
Jeferson Junio Menchik <juniomenchik@live.com>
2025-04-18 18:34:43 -0300

    [master] Adicionando classes iniciais.

---
+++ src/main/java/br/com/menchitech/ms_pdv_bms_restaurant/MsPdvBmsRestaurantApplication.java
@@ -0,0 +1,13 @@
+package br.com.menchitech.ms_pdv_bms_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPdvBmsRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPdvBmsRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/menchitech/ms_pdv_bms_restaurant/MsPdvBmsRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.menchitech.ms_pdv_bms_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPdvBmsRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
