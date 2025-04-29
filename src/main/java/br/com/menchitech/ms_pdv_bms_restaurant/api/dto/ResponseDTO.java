c6ff35f8de4276060ae1dd463b27ac1bec282063
Jeferson Junio Menchik <juniomenchik@live.com>
2025-04-29 16:27:05 -0300

    [master] Add non-null inclusion to ResponseDTO serialization
    This update ensures that only non-null fields are included in the JSON response, improving API response clarity and reducing payload size. The `@JsonInclude` annotation was applied to achieve this.

--- src/main/java/br/com/menchitech/ms_pdv_bms_restaurant/api/dto/ResponseDTO.java
+++ src/main/java/br/com/menchitech/ms_pdv_bms_restaurant/api/dto/ResponseDTO.java
@@ -1,10 +1,12 @@
 package br.com.menchitech.ms_pdv_bms_restaurant.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseDTO {
 
     private Object data;
